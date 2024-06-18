361a770d7daaf848570ef9739f89efe9e4c394fe
João de Lima <96394962+SpiderUntidy@users.noreply.github.com>
2024-06-18 16:03:06 -0300

    Update do gráfico empilhado - porcentagens

--- utils/graphPlot.py
+++ utils/graphPlot.py
@@ -87,11 +87,16 @@ def graficoEmpilhadoPlotly(data: dict):
     Returns:
         fig: Gráfico de barras empilhadas Plotly.
     """
-    df = pd.DataFrame(list(data.items()), columns=["Categories", "Quantity"])
+    df = pd.DataFrame(list(data.items()), columns=["Categories", ""])
     df.set_index("Categories", inplace=True)
     df = df.T
+    percentages = df.div(df.sum(axis=1), axis=0) * 100
     
-    fig = px.bar(df)
+    fig = px.bar(df, labels=dict(Quantity="", index="", value="Quantidade", Categories="Categorias"))
+    
+    for i, col in enumerate(df.columns):
+        fig.data[i].text = percentages[col].map('{:.2f}%'.format)
+        fig.data[i].textposition = 'inside'
     
     return fig
 
